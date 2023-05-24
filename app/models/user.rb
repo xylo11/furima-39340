a663afc97230c9b6c516dac85eb53692dd5d1aaa
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-05-24 17:13:11 +0900

    userモデルバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-ゞァ-ヴ一-龠々]+\z/u, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-ゞァ-ヴ一-龠々]+\z/u, message: "は全角（漢字・ひらがな・カタカナ）で入力してください" }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: "は全角（カタカナ）で入力してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: "は全角（カタカナ）で入力してください" }
+  validates :birth_date, presence: true
 end
