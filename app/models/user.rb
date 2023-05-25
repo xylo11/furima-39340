d42fa820d6aec7bf91abcdaba1ab0ad9fe5581f4
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-05-25 18:38:19 +0900

    正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: { message: 'を入力してください' }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合で入力してください' }
-  validates :family_name, presence: true, format: { with: /\A[ぁ-ゞァ-ヴ一-龠々]+\z/u, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-ゞァ-ヴ一-龠々]+\z/u, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: 'は全角（カタカナ）で入力してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: 'は全角（カタカナ）で入力してください' }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/u, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/u, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/u, message: 'は全角（カタカナ）で入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/u, message: 'は全角（カタカナ）で入力してください' }
   validates :birth_date, presence: true
 end
