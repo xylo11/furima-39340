4cc055f5ffbbd60fa483418a9738f3c0a6160944
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-05-28 21:33:53 +0900

    出品商品情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_description, 
+      :item_category_id, :item_condition_id, :item_shipping_method_id, 
+      :item_prefecture_id, :item_shipping_duration_id, 
+      :item_price).merge(user_id: current_user.id)
   end
 end
