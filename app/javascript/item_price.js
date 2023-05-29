0a475e60531050cd776be0b36932e167e9c668c6
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-05-29 17:39:49 +0900

    item-price要素がある場合発火

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,15 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
-  const taxOutput = document.getElementById("add-tax-price");
-  const profitOutput = document.getElementById("profit");
+window.addEventListener('DOMContentLoaded', () => {
+  if ( document.getElementById('item-price')!= null) {
+    const priceInput = document.getElementById("item-price");
+    const taxOutput = document.getElementById("add-tax-price");
+    const profitOutput = document.getElementById("profit");
 
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;
-    const outputTax = Math.floor(inputValue * 0.1);
-    const outputProfit = Math.floor(inputValue - outputTax);
-    taxOutput.innerHTML = outputTax;
-    profitOutput.innerHTML = outputProfit;
-  })
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const outputTax = Math.floor(inputValue * 0.1);
+      const outputProfit = Math.floor(inputValue - outputTax);
+      taxOutput.innerHTML = outputTax;
+      profitOutput.innerHTML = outputProfit;
+    })
+  }
 });
