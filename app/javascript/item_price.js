e8bce8fda0441c5c5ff3684537049990f90a5af8
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-05-29 16:56:49 +0900

    価格入力計算

---
+++ app/javascript/item_price.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  const taxOutput = document.getElementById("add-tax-price");
+  const profitOutput = document.getElementById("profit");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const outputTax = Math.floor(inputValue * 0.1);
+    const outputProfit = Math.floor(inputValue - outputTax);
+    taxOutput.innerHTML = outputTax;
+    profitOutput.innerHTML = outputProfit;
+  })
+});
