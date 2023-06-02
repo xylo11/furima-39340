72ecef4f9373cf56ee8cf13adb3a9a05365cb0cf
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-06-02 16:57:16 +0900

    DB作成購入記録・配送情報、記述ミス修正

--- db/migrate/20230601090926_create_shipping_informations.rb
+++ db/migrate/20230601090926_create_shipping_informations.rb
@@ -1,7 +1,13 @@
 class CreateShippingInformations < ActiveRecord::Migration[6.0]
   def change
-    create_table :shipping_informations do |t|
-
+    create_table   :shipping_informations do |t|
+      t.string     :postal_code,        null: false
+      t.integer    :item_prefecture_id, null: false
+      t.string     :city,               null: false
+      t.string     :street_address,     null: false
+      t.string     :building_name
+      t.string     :phone_number,       null: false
+      t.references :purchase_record,    null: false, foreign_key: true
       t.timestamps
     end
   end
