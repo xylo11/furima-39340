d6cb8826c4d4b818f17a508139facbea1b79e896
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-06-02 15:45:31 +0900

    Formオブジェクト、アクション

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,28 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_root, only: [:index, :create]
 
   def index
     @purchase_shipping = PurchaseShipping.new
   end
+
+  def create
+    @purchase_shipping = PurchaseShipping.new(purchase_shipping_params)
+    if @purchase_shipping.valid?
+      pay_item
+      @purchase_shipping.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_address_params
+    params.require(:purchase_shipping).permit(:postal_code, :item_prefecture_id, :city, :street_address, :building_name, 
+      :phone_number).merge(user_id: current_user.id, item_id: params[:item_id]
+    )
+  end
+
 end
